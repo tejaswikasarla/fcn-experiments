9cd98af592fcda42aacace140c8dc08f29f98795
Tejaswi Kasarla <tejaswikasarla@users.noreply.github.com>
2017-04-11 12:38:59 +0530

    Update entropy-heatmap.py

--- entropy-heatmap.py
+++ entropy-heatmap.py
@@ -2,24 +2,22 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-aa = net.blobs['score'].data[0]
-
 def softmax(z): 
     return np.exp(z) / np.sum(np.exp(z), axis=1, keepdims=True)
-    
-aasfmx = softmax(aa)
 
-lst = list(range(19))
+aa_out = net.blobs['score'].data[0] #last layer of fcn output
+aa_sfmx = softmax(aa_out)
+lst = list(range(19)) #no of classes of cityscapes dataset
 
+#find entropy of the image (of its prediction of all 19 classes from softmax
+# entropy is -sum_i(p_i *log p_i))
 entropy=0;
-
 for i in lst:
-    entropy = entropy+(aasfmx[i,:,:] * np.log2(aasfmx[i,:,:]))
-    
+    entropy = entropy+(aa_sfmx[i,:,:] * np.log2(aa_sfmx[i,:,:]))
 entropy = -1 * entropy; #entropy is -sum( pi * log pi)
-    
-#plt.imsave('image.png', entropy, cmap=plt.cm.jet) #save without colorbar reference
+
+#save without colorbar reference
+#plt.imsave('image.png', entropy, cmap=plt.cm.jet) 
 
 #save with colorbar reference
 fig,ax=plt.subplots(figsize=(20.48,10.24))
@@ -27,4 +25,3 @@ im=ax.imshow(entropy,cmap=plt.cm.jet)
 ax.axis('off')
 fig.colorbar(im,shrink=0.75)
 fig.savefig('images.png',bbox_inches='tight',pad_inches=0.0)
-
