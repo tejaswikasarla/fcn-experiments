79cba915ff0664d4b541aec1bdfb658b6edf6df9
Tejaswi Kasarla <tejaswikasarla@users.noreply.github.com>
2017-04-11 12:31:10 +0530

    Update entropy-heatmap.py to include colorbar ref.

--- entropy-heatmap.py
+++ entropy-heatmap.py
@@ -19,4 +19,12 @@ for i in lst:
     
 entropy = -1 * entropy; #entropy is -sum( pi * log pi)
     
-plt.imsave('image.png', entropy, cmap=plt.cm.jet)
+#plt.imsave('image.png', entropy, cmap=plt.cm.jet) #save without colorbar reference
+
+#save with colorbar reference
+fig,ax=plt.subplots(figsize=(20.48,10.24))
+im=ax.imshow(entropy,cmap=plt.cm.jet)
+ax.axis('off')
+fig.colorbar(im,shrink=0.75)
+fig.savefig('images.png',bbox_inches='tight',pad_inches=0.0)
+
